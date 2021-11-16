1731c18ba64cbca284c4c32c34d6dcab4445c9d7
AuroraArc <auroraarc04@gmail.com>
2021-11-15 23:27:20 -0600

    mdpt and trap project

---
+++ latexindent.pl-3.12/helper-scripts/update-version.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+generatePDFmode=0
+minorVersion=0
+while getopts "mp" OPTION
+do
+ case $OPTION in 
+  p)    
+   echo "re-generating pdf..."
+   generatePDFmode=1
+   ;;
+  m)    
+   echo "minor version, not removing most recently updated stars from documentation..."
+   minorVersion=1
+   ;;
+  ?)    printf "Usage: %s: [-s]  args\n" $(basename $0) >&2
+        exit 2
+        ;;
+ # end case
+ esac 
+done
+
+oldVersion='3.11'
+newVersion='3.12'
+oldDate='2021-07-31'
+newDate='2021-09-16'
+
+cd ../
+cd documentation
+[[ $minorVersion == 0 ]] && find -name "a*.tex" -print0|xargs -0 perl -p0i -e "s|announce\*\{|announce\{|sg"
+[[ $minorVersion == 0 ]] && find -name "s*.tex" -print0|xargs -0 perl -p0i -e "s|announce\*\{|announce\{|sg"
+find -name "a*.tex" -print0|xargs -0 perl -p0i -e "s|announce\{new|announce\*\{$newDate|sgi"
+find -name "s*.tex" -print0|xargs -0 perl -p0i -e "s|announce\{new|announce\*\{$newDate|sgi"
+cd ../
+sed -i.bak "s/version = u'$oldVersion'/version = u'$newVersion'/" documentation/conf.py
+sed -i.bak "s/release = u'$oldVersion'/release = u'$newVersion'/" documentation/conf.py
+sed -i.bak "s/\$versionNumber = '$oldVersion'/\$versionNumber = '$newVersion'/" LatexIndent/Version.pm
+sed -i.bak "s/\$versionDate = '$oldDate'/\$versionDate = '$newDate'/" LatexIndent/Version.pm
+sed -i.bak "s/version $oldVersion, $oldDate/version $newVersion, $newDate/" latexindent.pl
+sed -i.bak "s/version $oldVersion, $oldDate/version $newVersion, $newDate/" defaultSettings.yaml
+sed -i.bak "s/version $oldVersion,/version $newVersion,/" readme.md
+sed -i.bak "s/$oldDate/$newDate/" readme.md
+sed -i.bak "s/version $oldVersion,/version $newVersion,/" documentation/readme.txt
+sed -i.bak "s/version $oldVersion/version $newVersion/g" documentation/conf.py
+sed -i.bak "s/$oldDate/$newDate/" documentation/readme.txt
+sed -i.bak "s/Version $oldVersion/Version $newVersion/" documentation/title.tex
+sed -i.bak "s/$oldDate/$newDate/" documentation/title.tex
+sed -i.bak "s/\\documentclass\[10pt,draft\]/\\documentclass\[10pt\]/" documentation/latexindent.tex
+cd documentation
+## perl documentation-default-settings-update.pl -r
+egrep -i --color=auto 'announce{new' *.tex
+cd ../
+# possibly generate the pdf
+[[ $generatePDFmode == 1 ]] && cd documentation && arara latexindent
+exit
